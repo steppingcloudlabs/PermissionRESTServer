953a3cd69361ff49ae3c3c47affe52a0912259a8
Prakritidev Verma <prakritidevverma@gmail.com>
2020-02-25 18:15:47 +0530

    project init

---
+++ src/main/java/com/sclabs/TestingFakeMongoWebserver/PermissionRestServerApplication.java
@@ -0,0 +1,13 @@
+package com.sclabs.TestingFakeMongoWebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissionRestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sclabs/TestingFakeMongoWebserver/PermissionRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sclabs.TestingFakeMongoWebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
